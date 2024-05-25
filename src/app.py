83d6b61f0fbfa6970ecba7b8678fc06c0e0a977b
Yash <73090835+yashx420@users.noreply.github.com>
2024-05-25 15:41:10 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,61 +1,71 @@
-from flask import Flask, jsonify;
-import datetime
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-productList = [
-  {
-    "name": "Grey Hoodie",
-    "price": 25,
-    "image1": "https://m.media-amazon.com/images/I/61CgP6eiVvL._SX679_.jpg",
-    "image2": "https://m.media-amazon.com/images/I/71teqMgkimL._SY741_.jpg",
-    "soldOut": False,
-  },
-  {
-    "name": "Black Hoodie",
-    "price": 20,
-    "image1":
-      "https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/g/v/w/m-003-auraworld-original-imagjkj3hwumg8hu.jpeg?q=70",
-    "image2":
-      "https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/g/q/l/s-003-auraworld-original-imagjkj3kbnyqfhp.jpeg?q=70",
-    "soldOut": True,
-  },
-  {
-    "name": "Light-Green Sweatshirt",
-    "price": 22,
-    "image1":
-      "https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/k/k/k/s-hd-109-solid-thalasi-original-imagw6u4knzn2xys.jpeg?q=70",
-    "image2":
-      "https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/p/z/o/s-whd-204-thalasi-original-imagw5p97xgnxgan.jpeg?q=70",
-    "soldOut": False,
-  },
-  {
-    "name": "Purple Sweatshirt",
-    "price": 30,
-    "image1": "https://m.media-amazon.com/images/I/61x80IBNAQL._SY741_.jpg",
-    "image2": "https://m.media-amazon.com/images/I/61HmsZfSq1L._SY550_.jpg",
-    "soldOut": False,
-  },
-  {
-    "name": "Brown Fleece Sweatshirt",
-    "price": 25,
-    "image1": "https://m.media-amazon.com/images/I/61GKymVG6TL._SX569_.jpg",
-    "image2": "https://m.media-amazon.com/images/I/71S3I6m0A7L._SX569_.jpg",
-    "soldOut": True,
-  },
-  {
-    "name": "Dark Blue Sweatshirt",
-    "price": 15,
-    "image1": "https://m.media-amazon.com/images/I/41rPFtRyUvL._SX569_.jpg",
-    "image2": "https://m.media-amazon.com/images/I/71+rc05gXlL._SY741_.jpg",
-    "soldOut": False,
-  },
-]
+# Configure the database URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:iloveutuffy123@localhost:3306/products_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image1 = db.Column(db.String(255))
+    image2 = db.Column(db.String(255))
+    soldOut = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'image1': self.image1,
+            'image2': self.image2,
+            'soldOut': self.soldOut
+        }
+
+# Function to insert initial product data
+def insert_initial_data():
+    productList = []
+    
+
+    for product in productList:
+        new_product = Product(
+            name=product["name"],
+            price=product["price"],
+            image1=product["image1"],
+            image2=product["image2"],
+            soldOut=product["soldOut"]
+        )
+        db.session.add(new_product)
+    db.session.commit()
+
+with app.app_context():
+    db.create_all()
+    insert_initial_data()
 
 @app.route('/data', methods=['GET'])
-def get_data():
-    return jsonify(productList)
+def get_products():
+    products = Product.query.all()
+    return jsonify([product.to_dict() for product in products])
+
+@app.route('/data', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    new_product = Product(
+        name=data['name'],
+        price=data['price'],
+        image1=data['image1'],
+        image2=data['image2'],
+        soldOut=data['soldOut']
+    )
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify(new_product.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
-
